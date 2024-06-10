ad13c5fc96c954434ffccb7747704e6fe81c0e81
Nicolas Tobon <nicolastobon242>
2024-06-10 18:20:21 -0500

    Maintainability problem fixed

--- src/test/java/udem/edu/co/superheroes/SuperHeroesApplicationTests.java
+++ src/test/java/udem/edu/co/superheroes/SuperHeroesApplicationTests.java
@@ -1,13 +1,22 @@
 package udem.edu.co.superheroes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SuperHeroesApplicationTests {
 
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext, "The application context should have loaded.");
 	}
 
 }
